d87b8e902c45bf8508b551e4aff74c3238afacaf
Sunny Kumar <sunnyraj3600702@gmail.com>
2024-08-15 12:40:39 +0530

    Added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {Route, Routes } from 'react-router-dom';
 
 import AboutUs from './Pages/AboutUs';
+import Contact from './Pages/Contact';
 import CourseList from './Pages/Course/CourseList';
 import HomePage from './Pages/HomePage'
 import Login from './Pages/Login';
@@ -17,6 +18,7 @@ function App() {
           <Route path='/' element={<HomePage/>}></Route>
           <Route path='/about' element={<AboutUs/>}></Route>
           <Route path='/courses' element={<CourseList/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
           
           <Route path='/signup' element={<Signup/>}></Route>
           <Route path='/login' element={<Login/>}></Route>
