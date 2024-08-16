d38626ee034fe427e5a298f6e4692352fe768525
Sunny Kumar <sunnyraj3600702@gmail.com>
2024-08-16 12:06:49 +0530

    Added Edit Profile Page

--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -8,7 +8,8 @@ import axiosInstance from "../../Helpers/axiosInstance";
 const initialState={
     isLoggedIn:localStorage.getItem('isLoggedIn')||false,
     role:localStorage.getItem('role')||"",
-    data:JSON.parse (localStorage.getItem('data'))||{}
+    data: localStorage.getItem('data')!= undefined? JSON.parse(localStorage.getItem('data')):{}
+
 };
 
 export const createAccount=createAsyncThunk("/auth/signup",async(data)=>{
@@ -59,12 +60,41 @@ export const logout=createAsyncThunk("/auth/logout",async()=>{
         
         return (await res).data;
     } catch (error) {
-        toast.error(error?.response?.data?.message);
+        toast.error(error?.response?.data?.message);  
+    }
+});
 
 
+
+export const updateProfile=createAsyncThunk("/user/update/profile",async(data)=>{
+    try {
+        const res=axiosInstance.put(`user/update/${data[0]}`,data[1]);
+        toast .promise(res,{
+            loading:"Wait! Profile Update in progress...",
+            success:(data)=>{
+                return data ?. data?.message;
+            },
+            error: "Failed to Update Profile",
+        });
         
+        return (await res).data;
+    } catch (error) {
+        toast.error(error?.response?.data?.message);  
     }
 })
+
+
+
+export const getUserData=createAsyncThunk("/user/details",async()=>{
+    try {
+        const res=axiosInstance.get("user/me");
+        return (await res).data;
+    } catch (error) {
+        toast.error(error.message);  
+    }
+})
+
+
 
 
 
@@ -87,6 +117,15 @@ const authSlice=createSlice({
             state.data={};
             state.isLoggedIn=false;
             state.role="";
+        })
+        .addCase(getUserData.fulfilled,(state,action)=>{
+            if(!action?.payload?.user) return;
+            localStorage.setItem("data",JSON.stringify(action?.payload?.user));
+            localStorage.setItem("isLoggedIn",true);
+            localStorage.setItem("role",action?.payload?.user?.role);
+            state.isLoggedIn=true;
+            state.data=action?.payload?.user;
+            state.role=action?.payload?.user?.role;
         })
     }
 });
