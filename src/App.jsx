e275a1f6cc1c70f4d7811ccd789b35ce46d96428
Sunny Kumar <sunnyraj3600702@gmail.com>
2024-09-07 15:33:38 +0530

    Added payment cancelation and cheakout fail

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import HomePage from './Pages/HomePage'
 import Login from './Pages/Login';
 import NotFound from './Pages/NotFound';
 import Checkout from './Pages/Payment/Checkout';
+import CheckoutFailure from './Pages/Payment/CheckoutFailure';
 import CheckoutSuccess from './Pages/Payment/CheckoutSuccess';
 import Signup from './Pages/Signup';
 import EditProfile from './Pages/User/EditProfile';
@@ -46,6 +47,9 @@ function App() {
               <Route path='/user/editprofile' element={<EditProfile/>}/>
               <Route path='/checkout' element={<Checkout/>}/>
               <Route path='/checkout/success' element={<CheckoutSuccess/>}/>
+              <Route path='/checkout/fail' element={<CheckoutFailure/>}/>
+
+
 
 
 
