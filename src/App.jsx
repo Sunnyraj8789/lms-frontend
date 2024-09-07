e1ec6834229aa2d729c5f5a5a72a244bbf7fd177
Sunny Kumar <sunnyraj3600702@gmail.com>
2024-09-07 13:08:38 +0530

    Added complete cheakout page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import HomePage from './Pages/HomePage'
 import Login from './Pages/Login';
 import NotFound from './Pages/NotFound';
 import Checkout from './Pages/Payment/Checkout';
+import CheckoutSuccess from './Pages/Payment/CheckoutSuccess';
 import Signup from './Pages/Signup';
 import EditProfile from './Pages/User/EditProfile';
 import Profile from './Pages/User/Profile';
@@ -44,6 +45,8 @@ function App() {
               <Route path='/user/profile' element={<Profile/>}/>
               <Route path='/user/editprofile' element={<EditProfile/>}/>
               <Route path='/checkout' element={<Checkout/>}/>
+              <Route path='/checkout/success' element={<CheckoutSuccess/>}/>
+
 
 
 
