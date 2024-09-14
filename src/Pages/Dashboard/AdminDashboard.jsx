8bcf0317d8c0fac97a7ffd873f7d68d03d680103
Sunny Kumar <sunnyraj3600702@gmail.com>
2024-09-14 23:26:43 +0530

    added admin dashboard

--- src/Pages/Dashboard/AdminDashboard.jsx
+++ src/Pages/Dashboard/AdminDashboard.jsx
@@ -141,7 +141,7 @@ function AdminDashboard(){
                 </div>
 
                 <div className="mx-[10%] w-[80%] self-center flex flex-col items-center justify-center gap-10 mb-10">
-                        <div className="flex w-full items-center justify-center">
+                        <div className="flex w-full items-center justify-between">
                             <h1 className="text-center text-3xl font-semibold">
                                         Courses Overview
                             </h1>
