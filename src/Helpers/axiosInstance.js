ad4426dcd86fdda5ccaa2689766bd95754a8fc6e
Sunny Kumar <141814845+Sunnyraj8789@users.noreply.github.com>
2024-09-15 21:24:54 +0530

    Update axiosInstance.js

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL="https://lms-server-qa4x.onrender.com";
+const BASE_URL="https://lms-server-qa4x.onrender.com/api/v1";
 
 const axiosInstance=axios.create();
 
